395bf10328a5b6818bc507d731bddf15ffb7b4fc
OTTO <otto@MacBook-Air-de-OTTO.local>
2023-08-24 22:13:30 -0300

    Exportando e importando bibliotecas e arquivos

---
+++ primeiro-projeto-npm/array.js
@@ -0,0 +1,8 @@
+const arrayNumeros = [1, 3, 6, 9, 11];
+
+const arrayLetras = ['a', 'b', 'c', 'd'];
+
+module.exports = {
+    arrayNumeros,
+    arrayLetras
+};
---
+++ primeiro-projeto-npm/index.js
@@ -0,0 +1,13 @@
+const lodash = require('lodash'); // Importar biblioteca completa
+// const { uniq } = require('lodash') // --> Desestruturação (mesmo resultado)
+const { arrayNumeros, arrayLetras } = require('./array');
+
+// const array = [1, 2, 4, 6, 4, 9, 7, 1];
+
+// const araryUnico = lodash.uniq(array);
+
+// console.log(araryUnico);
+
+// console.log(array)
+
+console.log(arrayLetras, arrayNumeros)
