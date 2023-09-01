ef9fee47b0b176bf4210c3d0ce633c12257b33d7
OTTO <otto@MacBook-Air-de-OTTO.local>
2023-09-01 19:49:22 -0300

    Salvando projeto

--- primeiro-servidor-express/src/index.js
+++ primeiro-servidor-express/src/index.js
@@ -2,8 +2,32 @@ const express = require('express');
 
 const app = express();
 
-app.get('/home', (requisicao, resposta) => {
-    resposta.send('Cara, que aula empolgante!');
+app.get('/usuario', (requisicao, res) => {
+    let usuarios = [
+        { id: 11, nome: 'joao', idade: 23 },
+        {
+            id: 2,
+            nome: 'maria',
+            idade: 18
+        },
+        {
+            id: 4,
+            nome: 'ana',
+            idade: 30
+        },
+        {
+            id: 1,
+            nome: 'rodrigo',
+            idade: 17
+        },
+        {
+            id: 123,
+            nome: 'rodrigo',
+            idade: 17
+        }]
+    res.send(usuarios);
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('servidor inicializado na porta 3000')
+});
