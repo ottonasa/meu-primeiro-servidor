9f4c708e2fd07adc6d3671563f3217bc4125f5c5
OTTO <otto@MacBook-Air-de-OTTO.local>
2023-09-02 17:25:05 -0300

    Intermediarios

--- src/controladores/professores.js
+++ src/controladores/professores.js
@@ -4,6 +4,8 @@ const filtrarProfessores = (req, res) => {
     const { stack } = req.query;
     let resultado = professores;
 
+    console.log('cheguei no controlador de listagem do professor')
+
     if (stack) {
         resultado = professores.filter((professor) => {
             return professor.stack === stack
@@ -13,6 +15,7 @@ const filtrarProfessores = (req, res) => {
 }
 
 const encontrarProfessor = (req, res) => {
+    console.log('rota para encontrar um professor')
     const professorEncontrado = professores.find((professor) => {
         return professor.id === Number(req.params.id)
     });
--- src/index.js
+++ src/index.js
@@ -4,8 +4,23 @@ const { filtrarProfessores, encontrarProfessor } = require('./controladores/prof
 
 const app = express();
 
+const primeiroIntermediario = (req, res, next) => {
+    console.log('passei no primeiro intermediário');
+    next();
+};
+const segundoIntermediario = (req, res, next) => {
+    console.log('passei no segundo intermediário');
+    next();
+};
+const intermediarioDaRota = (req, res, next) => {
+    console.log('passei no intermediário da rota');
+    next();
+};
 
-app.get('/professores', filtrarProfessores)
+// app.use(primeiroIntermediario);
+// app.use(segundoIntermediario);
+
+app.get('/professores', intermediarioDaRota, filtrarProfessores)
 
 app.get('/professores/:id', encontrarProfessor)
 
